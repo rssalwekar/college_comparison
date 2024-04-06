26a1094769bc392c61a7dea27055c07e5b1bd2f9
aesareen <arnavsareen@gmail.com>
2024-04-06 13:51:19 -0400

    rmp commit

---
+++ COLLEGES.py
@@ -0,0 +1,15 @@
+COLLEGES = {
+    "University of North Carolina at Charlotte": {
+        "catalog": "https://catalog.charlotte.edu/mime/media/10/5816/2023-2024-Undergraduate-Catalog.pdf",
+        "rmp_id": 1253,
+        "department_links": {
+            'biology': 'https://www.ratemyprofessors.com/search/professors/1253?q=*&did=6',
+             'business': 'https://www.ratemyprofessors.com/search/professors/1253?q=*&did=7',
+             'chemistry': 'https://www.ratemyprofessors.com/search/professors/1253?q=*&did=8',
+        },
+    },
+    "University of North Carolina": {
+        "catalog": "https://catalog.unc.edu/pdf/2023-24-ugrad.pdf",
+        "rmp_id": 1232
+    }
+    }
